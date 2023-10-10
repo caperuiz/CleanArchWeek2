f1d07be99d5052888359bddd314a21d80de4269c
cpena <cpena@oneinc.com>
2023-10-10 15:13:02 -0500

    data annotations

--- CatalogService.Domain/Entities/Category.cs
+++ CatalogService.Domain/Entities/Category.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CatalogService.Domain.Entities
 {
     public class Category
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
         public string Name { get; set; }
-        public string ImageUrl { get; set; } 
+
+        [Url(ErrorMessage = "Image must be a valid URL.")]
+        public string Image { get; set; }
     }
 }
--- CatalogService.Domain/Entities/Item.cs
+++ CatalogService.Domain/Entities/Item.cs
@@ -1,20 +1,38 @@
 ﻿// CatalogService.Domain/Entities/Item.cs
 
+using CatalogService.Domain.Entities;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CatalogService.Domain.Entities
 {
     public class Item
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
         public string Name { get; set; }
+
+        [MaxLength(4000)] // Adjust the max length as needed
         public string Description { get; set; }
+
+        [Url(ErrorMessage = "Image must be a valid URL.")]
         public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
         public int CategoryId { get; set; }
-        public decimal Price { get; set; }
-        public int Amount { get; set; }
 
         public virtual Category Category { get; set; }
-        // Add navigation properties or other properties as needed
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Amount is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Amount must be a positive integer.")]
+        public int Amount { get; set; }
     }
 }
+
+
